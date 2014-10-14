a907409f6347225569945fe841ee665c448e8056
Chaos <cawiii@me.com>
2014-10-14 08:07:38 -0400

    can partially display a frame, need to do yuv colorspace converison

--- main.cpp
+++ main.cpp
@@ -1,8 +1,45 @@
 #include "main.h"
+#include "zbmp.h"
 
-void *decoderCallback(AVFrame *frame, AVPacket *pkt, void *user){
-    LOG("Read frame");
-    return NULL;
+#include <libavcodec/avcodec.h>
+#include <libavutil/avutil.h>
+
+void SaveFrame(AVFrame *pFrame, int width, int height, int iFrame)
+{
+    FILE *pFile;
+    char szFilename[32];
+    int  y;
+
+    // Open file
+    sprintf(szFilename, "frame.ppm", iFrame);
+    pFile=fopen(szFilename, "wb");
+    if(pFile==NULL)
+        return;
+
+    // Write header
+    fprintf(pFile, "P6\n%d %d\n255\n", width, height);
+
+    // Write pixel data
+    for(y=0; y<height; y++)
+        fwrite(pFrame->data[0]+y*pFrame->linesize[0], 1, width*3, pFile);
+
+    // Close file
+    fclose(pFile);
+}
+
+void decoderCallback(AVFrame *frame, AVPacket *pkt, void *user){
+    LOG("Read frame: " << frame->width << " " << frame->height << " " << frame->coded_picture_number << " " << frame->display_picture_number);
+    if(frame->coded_picture_number == 283){
+        ZImage image((zu64)frame->width, (zu64)frame->height);
+        image.newData();
+        memcpy(image.buffer(), frame->data[0], frame->linesize[0]);
+        memcpy(image.buffer(), frame->data[1], frame->linesize[1]);
+        memcpy(image.buffer(), frame->data[2], frame->linesize[2]);
+        ZBMP bmp(image);
+        LOG(bmp.write("out.bmp"));
+
+        SaveFrame(frame, frame->width, frame->height, frame->coded_picture_number);
+    }
 }
 
 int main(){
@@ -10,22 +47,21 @@ int main(){
     ZLog::formatStderr(ZLogSource::error, "%time% %thread% %function% (%file%:%line%) - %log%");
     LOG("Starting H264-ROI");
 
-    H264_Decoder decoder((h264_decoder_callback)decoderCallback, NULL);
+    H264_Decoder decoder(decoderCallback, &decoder);
 
 #if PLATFORM == LINUX
-    //decoder.load("/home/chaos/Shared/bf4 hs2.mp4");
-    decoder.load("/home/chaos/Shared/sao1.h264");
+    //LOG("Load: " << decoder.load("/home/chaos/Shared/bf4 hs2.mp4"));
+    LOG("Load: " << decoder.load("/home/chaos/Shared/sao1.h264"));
 #else
-    //decoder.load("F:\\Video\\bf4 hs2.mp4");
-    decoder.load("E:\\Users\\Chaos\\Shared\\sao1.h264");
+    //LOG("Load: " << decoder.load("F:\\Video\\bf4 hs2.mp4"));
+    LOG("Load: " << decoder.load("E:\\Users\\Chaos\\Shared\\sao1.h264"));
+    //LOG("Load: " << decoder.load(rx_to_data_path("sao1.h264"), 30.0f));
 #endif
 
     LOG("Reading frames...");
 
-    bool ok = true;
-    while(ok){
-        ok = decoder.readFrame();
-        LOG(ok);
+    while(true){
+        decoder.readFrame();
     }
 
     //X264Encoder encoder;
