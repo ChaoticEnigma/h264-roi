8286bd5376c7c67111dffb79ebfdc6d00ba68b10
Chaos <cawiii@me.com>
2014-12-04 04:07:09 -0500

    image downloader is cleaner

--- util/imagedownloader.cpp
+++ util/imagedownloader.cpp
@@ -1,6 +1,7 @@
 #include "zlog.h"
 #include "zfile.h"
 #include "zbinary.h"
+#include "zlist.h"
 using namespace LibChaos;
 
 // Download multiple images from internet
@@ -98,42 +99,86 @@ ZString getPixivPage(ZString url, ZString session, ZString args = ""){
     return doc;
 }
 
-bool savePixivImage(ZString id, ZString user, ZString session, ZString &file){
+struct PixivImage {
+    ZString title;
+    ZString url;
+    ZString refer;
+};
+
+ZArray<PixivImage> getPixivIllustration(ZString id, ZString user, ZString session){
     ZString mediumurl = "http://www.pixiv.net/member_illust.php?mode=medium&illust_id=" + id;
 //    ZString bigurl = "http://www.pixiv.net/member_illust.php?mode=big&illust_id=" + id;
 
     ZString medpage = getPixivPage(mediumurl, session);
-    ZString refbigurl = "http://www.pixiv.net/" + medpage.findFirstBetween("<div class=\"works_display\"><a href=\"", "\" target=\"_blank\" class=\"");
-    refbigurl.replace("&amp;", "&");
+    ZString bigurl = "http://www.pixiv.net/" + medpage.findFirstBetween("<div class=\"works_display\"><a href=\"", "\" target=\"_blank\" class=\"");
+    bigurl.replace("&amp;", "&");
 
     ZString pixivrefer = "--header=\"Referer: " + mediumurl + "\"";
-    ZString imgpage = getPixivPage(refbigurl, session, pixivrefer);
+    ZString bigpage = getPixivPage(bigurl, session, pixivrefer);
 
-    ZString imgurl;
-    ZString type = refbigurl.findFirstBetween("mode=", "&");
+    ZArray<PixivImage> urls;
+    ZString type = bigurl.findFirstBetween("mode=", "&");
     if(type == "big"){
-        imgurl = imgpage.findFirstBetween("</head><body><img src=\"", "\" onclick=\"");
+        LOG("BIG");
+        PixivImage pix;
+        pix.title = id;
+        pix.url = bigpage.findFirstBetween("</head><body><img src=\"", "\" onclick=\"");
+        pix.refer = bigurl;
+        urls.push(pix);
+//        return savePixivImage(imgurl, refbigurl, user, session, file);
     } else if(type == "manga"){
-        imgurl = imgpage.findFirstBetween("data-filter=\"manga-image\" data-src=\"", "\" data-index=\"");
-    }
+        LOG("MANGA");
+        ZString search = "data-filter=\"manga-image\" data-src=\"";
+        ZArray<zu64> imgs = bigpage.findAll(search);
+        for(zu64 i = 0; i < imgs.size(); ++i){
+            ZString after = ZString::substr(bigpage, imgs[i] + search.size());
+            ZString url = ZString::getUntil(after, "\" data-index=\"");
+            PixivImage pix;
+            pix.title = id + " #" + (i+1);
+            pix.url = url;
+            pix.refer = bigurl;
+            urls.push(pix);
+        }
 
+//        imgurl = bigpage.findFirstBetween("data-filter=\"manga-image\" data-src=\"", "\" data-index=\"");
+//        zu64 pos = imgurl.findFirst("_p");
+//        ZString first = ZString::substr(imgurl, 0, pos + 2);
+//        ZString last = ZString::substr(imgurl, pos + 3);
+
+//        int num = 0;
+//        while(true){
+//            ZString mangaurl = first + num + last;
+//            ++num;
+//            if(!savePixivImage(mangaurl, bigurl, user, session, file))
+//                break;
+//            urls.push({ mangaurl, bigurl });
+//            LOG(mangaurl);
+//        }
+//        return num > 0;
+    }
+    return urls;
+}
+
+bool savePixivImage(ZString url, ZString refer, ZString user, ZString session, ZString file){
     ZString agent = "--user-agent=\"Mozilla/5.0 (Windows NT 6.1; WOW64; rv:33.0) Gecko/20100101 Firefox/33.0\"";
     ZString cookies = "--no-cookies --header=\"Cookie: PHPSESSID=" + session + "\"";
 //    ZString headers = "--header=\"Host: i4.pixiv.net\"";
-    ZString headers = "--header=\"Host: " + imgurl.explode('/')[1] + "\"";
-    ZString referer = "--header=\"Referer: " + refbigurl + "\"";
-    ZString command = "wget --no-check-certificate " + /*agent + " " +*/ cookies + " " + headers + " " + referer + " -P " + user + " \"" + imgurl + "\"";
+    ZString headers = "--header=\"Host: " + url.explode('/')[1] + "\"";
+    ZString referer = "--header=\"Referer: " + refer + "\"";
+    ZString command = "wget --no-check-certificate " + /*agent + " " +*/ cookies + " " + headers + " " + referer + " -O \"" + (ZPath(user) + (file + ZPath(url).getExtension())).str() + "\" \"" + url + "\"";
+    // -P \"" + user + "\"
 
     if(system((command + "1> NUL 2> NUL").cc()) != 0){
         LOG("Failed: " << command);
         return false;
     } else {
-        file = (ZPath(user) + ZPath(imgurl).last()).str();
         return true;
     }
 }
 
 bool pixivDownload(ZString user_id, ZString session){
+    ZFile::makeDir(user_id);
+
     ArZ imglist;
     bool any = true;
     for(zu64 i = 1; any; ++i){
@@ -159,60 +204,78 @@ bool pixivDownload(ZString user_id, ZString session){
     }
     LOG("Downloading " << imglist.size() << " images");
 
+    ZArray<PixivImage> imgurls;
+
     for(zu64 i = 0; i < imglist.size(); ++i){
         ZString item = imglist[i];
-        ZString file;
-
+        zu64 count = 0;
         int failcount = 0;
-        while(!savePixivImage(item, user_id, session, file) && failcount < 5){
+        while(failcount < 5){
+            ZArray<PixivImage> imgs = getPixivIllustration(item, user_id, session);
+            count = imgs.size();
+            for(zu64 j = 0; j < imgs.size(); ++j){
+                imgurls.push(imgs[j]);
+            }
+            if(!imgs.isEmpty())
+                break;
             ++failcount;
-            LOG(i+1 << "/" << imglist.size() << " retrying " << item);
+            LOG(i+1 << "/" << imglist.size() << " Retrying " << item);
         }
 
         if(failcount == 0){
-            LOG(i+1 << "/" << imglist.size() << " Saved " << item << " as " << file);
+            LOG(i+1 << "/" << imglist.size() << " Found " << count << " in " << item);
         } else {
-            LOG(i+1 << "/" << imglist.size() << " Failed to download " << item);
+            LOG(i+1 << "/" << imglist.size() << " Failed to list " << item);
         }
     }
+
+    for(zu64 i = 0; imgurls.size(); ++i){
+        PixivImage img = imgurls[i];
+        if(savePixivImage(img.url, img.refer, user_id, session, img.title)){
+            LOG(i+1 << "/" << imgurls.size() << " Saved " << img.title);
+        } else {
+            LOG(i+1 << "/" << imgurls.size() << " Failed to download " << img.title);
+        }
+    }
+
     return true;
 }
 
 bool pixivBookmarksDownload(ZString user_id, ZString session){
-    ArZ imglist;
-    bool any = true;
-    for(zu64 i = 1; any; ++i){
-        any = false;
-        ZString page = getPixivPage("http://www.pixiv.net/bookmark.php?id=" + user_id + "&rest=show&p=" + i, session);
-        page = page.findFirstBetween("<div class=\"\"><ul class=\"_image-items\">", "</ul></div><div class=\"clear\"></div><ul class=\"column-order-menu\"><div class=\"pager-container\">");
-        ArZ list = page.strExplode("<li class=\"image-item\" id=\"li_");
-        zu64 count = 0;
-        for(zu64 j = 0; j < list.size(); ++j){
-            ZString item = list[j];
-            item.substr(12);
-            ZString test = "<a href=\"member_illust.php?mode=medium&amp;illust_id=";
-            if(item.startsWith(test)){
-                item.substr(test.size());
-                item = ZString::getUntil(item, "\"");
-                if(item.isInteger()){
-                    imglist.push(item);
-                    ++count;
-                    any = true;
-                }
-            }
-        }
-        LOG("Read page " << i << " - " << count);
-    }
-    LOG("Downloading " << imglist.size() << " images");
+//    ArZ imglist;
+//    bool any = true;
+//    for(zu64 i = 1; any; ++i){
+//        any = false;
+//        ZString page = getPixivPage("http://www.pixiv.net/bookmark.php?id=" + user_id + "&rest=show&p=" + i, session);
+//        page = page.findFirstBetween("<div class=\"\"><ul class=\"_image-items\">", "</ul></div><div class=\"clear\"></div><ul class=\"column-order-menu\"><div class=\"pager-container\">");
+//        ArZ list = page.strExplode("<li class=\"image-item\" id=\"li_");
+//        zu64 count = 0;
+//        for(zu64 j = 0; j < list.size(); ++j){
+//            ZString item = list[j];
+//            item.substr(12);
+//            ZString test = "<a href=\"member_illust.php?mode=medium&amp;illust_id=";
+//            if(item.startsWith(test)){
+//                item.substr(test.size());
+//                item = ZString::getUntil(item, "\"");
+//                if(item.isInteger()){
+//                    imglist.push(item);
+//                    ++count;
+//                    any = true;
+//                }
+//            }
+//        }
+//        LOG("Read page " << i << " - " << count);
+//    }
+//    LOG("Downloading " << imglist.size() << " images");
 
-    for(zu64 i = 0; i < imglist.size(); ++i){
-        ZString item = imglist[i];
-        ZString file;
-        if(savePixivImage(item, user_id, session, file)){
-            LOG(i+1 << "/" << imglist.size() << " Saved " << item << " as " << file);
-        } else {
-            LOG(i+1 << "/" << imglist.size() << " Failed to download " << item);
-        }
-    }
+//    for(zu64 i = 0; i < imglist.size(); ++i){
+//        ZString item = imglist[i];
+//        ZString file;
+//        if(savePixivIllustration(item, user_id, session, file)){
+//            LOG(i+1 << "/" << imglist.size() << " Saved " << item << " as " << file);
+//        } else {
+//            LOG(i+1 << "/" << imglist.size() << " Failed to download " << item);
+//        }
+//    }
     return true;
 }
