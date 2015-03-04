1711799cced3bdbbb399254bb6fe7d6c04e64c3c
Chaos VM <cawiii@me.com>
2015-03-04 13:58:12 -0500

    wrapper script

--- roi.sh
+++ roi.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
-./h264_roi $*
+
+i=0
+for var in "$@"
+do
+    echo "$var"
+    args[i]="$var"
+    ((++i))
+done
+
+ffmpeg -i "$1" -c:v libx264 -pix_fmt yuv420p -preset slow -qp 10 -an "$1.h264"
+ffmpeg -i "$1" -vn -c:a copy "$1.audio"
+
+args[0]
+
+echo ${args[@]}
+#./h264_roi ${args[@]}
+
+ffmpeg -i "$1.roi.h264" -i "$1.audio" -c:v copy -c:a copy "$2"
