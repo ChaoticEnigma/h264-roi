5bd6ce631f8b4ed6849e9874762cb52a17ecac05
Chaos VM <cawiii@me.com>
2015-03-05 00:17:51 -0500

    roi.sh untested but done

--- roi.sh
+++ roi.sh
@@ -7,13 +7,16 @@ do
     ((++i))
 done
 
-ffmpeg -i "$1" -c:v libx264 -pix_fmt yuv420p -preset slow -qp 10 -an "$1.h264"
-ffmpeg -i "$1" -vn -c:a copy "$1.audio"
+args[0]="\"$1.h264\""
+args[1]="\"$1.roi.h264\""
 
-args[0]="$1.h264"
-args[1]="$1.roi.h264"
+echo ffmpeg -i "$1" -c:v libx264 -pix_fmt yuv420p -preset slow -qp 10 -an "$1.h264"
+echo ffmpeg -i "$1" -vn -c:a copy "$1.audio"
+#ffmpeg -i "$1" -c:v libx264 -pix_fmt yuv420p -preset slow -qp 10 -an "$1.h264"
+#ffmpeg -i "$1" -vn -c:a copy "$1.audio"
 
-echo ${args[@]}
+echo h264_roi ${args[@]}
 #./h264_roi ${args[@]}
 
-ffmpeg -i "$1.roi.h264" -i "$1.audio" -c:v copy -c:a copy "$2"
+echo ffmpeg -i "$1.roi.h264" -i "$1.audio" -c:v copy -c:a copy "$2"
+#ffmpeg -i "$1.roi.h264" -i "$1.audio" -c:v copy -c:a copy "$2"
