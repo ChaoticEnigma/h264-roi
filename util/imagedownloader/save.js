ba513a8580b4e2e6ec9b7fbffa5e452fc710585a
Chaos <cawiii@me.com>
2015-04-05 07:53:42 -0400

    add deviantart and furaffinity to imagedownloader save

--- util/imagedownloader/save.js
+++ util/imagedownloader/save.js
@@ -16,18 +16,19 @@
 
     var sendSave = function(record){
         var outrecord = {};
-        if(record.id)           outrecord.id        = record.id;
-        if(record.score)        outrecord.score     = record.score;
-        if(record.parent)       outrecord.parent    = record.parent;
-        if(record.artistid)     outrecord.artistid  = record.artistid;
-        if(record.artist)       outrecord.artist    = record.artist;
-        if(record.title)        outrecord.title     = record.title;
-        if(record.tags)         outrecord.tags      = record.tags;
-        if(record.character)    outrecord.character = record.character;
-        if(record.alltags)      outrecord.alltags   = record.alltags;
-        if(record.url)          outrecord.url       = record.url;
-        if(record.session)      outrecord.session   = record.session;
-        if(record.type)         outrecord.type      = record.type;
+        if(record.id)               outrecord.id        = record.id;
+        if(record.score)            outrecord.score     = record.score;
+        if(record.parent)           outrecord.parent    = record.parent;
+        if(record.artistid)         outrecord.artistid  = record.artistid;
+        if(record.artist)           outrecord.artist    = record.artist;
+        if(record.title)            outrecord.title     = record.title;
+        if(record.category)         outrecord.category  = record.category;
+        if(record.tags)             outrecord.tags      = record.tags;
+        if(record.character)        outrecord.character = record.character;
+        if(record.alltags)          outrecord.alltags   = record.alltags;
+        if(record.url)              outrecord.url       = record.url;
+        if(record.session)          outrecord.session   = record.session;
+        if(record.type)             outrecord.type      = record.type;
         outrecord.site = record.site;
 
         var json = JSON.stringify(outrecord);
@@ -50,10 +51,10 @@
             request.send(json);
         }
     };
-    
+
     var save_e621 = function($, record){
         record.site = "e621";
-        
+
         // Get post ID
         var id = $("div#stats ul li:contains('ID:')").html();
         record.id = id.substring(id.indexOf(' ') + 1);
@@ -63,7 +64,7 @@
         if(parentid){
             record.parent = parentid.replace("/post/show/", "");
         }
-        
+
         // Get image URL
         record.url = $("div#right-col h4 a:last").attr("href");
 
@@ -81,10 +82,10 @@
         if(characters.length){
             record.character = characters;
         }
-        
+
         // Custom search tags
         var findtags = ["breasts", "nipples", "pussy", "sex", "oral", "anal", "vaginal", "penetration", "male/female", "female/female"];
-        
+
         // Get all other tags
         record.tags = [];
         record.alltags = $("ul#tag-sidebar li:not(.tag-type-artist,.tag-type-character) a:nth-child(2)").map(function(){
@@ -94,27 +95,27 @@
                 record.tags.push(unescape(tag))
             return unescape(tag);
         }).get();
-        
+
         console.log(record.site + " - Score: " + record.score + ", ID: " + record.id);
         sendSave(record);
     };
-    
+
     var save_konachan = function($, record){
         record.site = "konachan";
-        
+
         // Get post ID
         var id = $("div#stats ul li:contains('Id:')").html();
         record.id = id.substring(id.indexOf(' ') + 1);
-        
+
         // Get parent post ID
         var parentid = $("div#post-view div.status-notice a:contains('parent post')").attr("href");
         if(parentid){
             record.parent = parentid.replace("/post/show/", "");
         }
-        
+
         // Get image URL
         record.url = $("div#post-view li a#highres").attr("href");
-        
+
         // Get Artist tags
         record.artist = $("ul#tag-sidebar li.tag-type-artist a:nth-child(2)").map(function(){
             var str = $(this).attr("href");
@@ -129,10 +130,10 @@
         if(characters.length){
             record.character = characters;
         }
-        
+
         // Custom search tags
         var findtags = ["censored", "uncensored", "breasts", "nipples", "pussy", "sex"];
-        
+
         // Get all other tags
         record.tags = [];
         record.alltags = $("ul#tag-sidebar li:not(.tag-type-artist,.tag-type-character) a:nth-child(2)").map(function(){
@@ -142,27 +143,27 @@
                 record.tags.push(unescape(tag))
             return unescape(tag);
         }).get();
-        
+
         console.log(record.site + " - Score: " + record.score + ", ID: " + record.id);
         sendSave(record);
     };
-    
+
     var save_sankaku = function($, record){
         record.site = "sankaku";
-        
+
         // Get post ID
         var id = $("h1#site-title a:nth-child(2)").attr("href");
         record.id = id.replace("/post/show/", "");
-        
+
         // Get parent post ID
         var parentid = $("div#post-view div.status-notice a:contains('parent post')").attr("href");
         if(parentid){
             record.parent = parentid.replace("/post/show/", "");
         }
-        
+
         // Get image URL
         record.url = "https:" + $("div#post-view li a#highres").attr("href");
-        
+
         // Get Artist tags
         var artists = $("ul#tag-sidebar li.tag-type-artist > a").map(function(){
             var str = $(this).attr("href");
@@ -180,10 +181,10 @@
         if(characters.length){
             record.character = characters;
         }
-        
+
         // Custom search tags
         var findtags = ["censored", "uncensored", "cleavage", "breasts", "nipples", "vagina", "sex"];
-        
+
         // Get all other tags
         record.tags = [];
         record.alltags = $("ul#tag-sidebar li:not(.tag-type-artist,.tag-type-character) > a").map(function(){
@@ -193,25 +194,25 @@
                 record.tags.push(unescape(tag))
             return unescape(tag);
         }).get();
-        
+
         console.log(record.site + " - Score: " + record.score + ", ID: " + record.id);
         sendSave(record);
     };
-    
+
     var save_pixiv = function($, record){
         record.site = "pixiv";
-        
+
         // Get illustration ID
         record.id = location.href.substring(location.href.indexOf("illust_id=") + 10);
-        
+
         // Get artist info
         var artisturl = $('div.profile-unit a.user-link').attr('href');
         record.artistid = artisturl.substring(artisturl.indexOf("id=") + 3);
         record.artist = $('div.profile-unit a.user-link h1.user').html();
-        
+
         // Custom search tags
         var findtags = ["R-18"];
-        
+
         // Get all other tags
         record.tags = [];
         record.alltags = $("section.work-tags span.tags-container ul.tags li.tag a.text").map(function(){
@@ -220,14 +221,14 @@
                 record.tags.push(tag)
             return tag;
         }).get();
-        
+
         // Get URL(s)
         if($("div.works_display div.ui-modal-trigger").size()){ // Modal Preview
             var url = $("div._illust_modal img.original-image").attr("data-src");
             record.url = [url];
             record.session = { "Referer" : location.href };
             record.type = "illustration";
-            
+
             console.log(record.site + " - Type: " + record.type + ", Score: " + record.score + ", ID: " + record.id);
             sendSave(record);
         } else { // Manga Preview
@@ -244,35 +245,102 @@
                 record.url = urls;
                 record.session = { "Referer" : location.protocol + "//" + location.host + "/" + previewpath };
                 record.type = "manga";
-                
+
                 console.log(record.site + " - Type: " + record.type + ", Score: " + record.score + ", ID: " + record.id);
                 sendSave(record);
             });
         }
     };
-    
+
     var save_deviantart = function($, record){
-        
         record.site = "deviantart";
-        
+
         var dlbutton = $("a.dev-page-button.dev-page-download");
-        record.id = dlbutton.attr("data-deviationid"); // ID
-        record.url = dlbutton.attr("href"); // URL
-        
-        record.artist = location.hostname.substring(0, location.hostname.indexOf(".") - 1); // Artist
-        record.title = $("div.dev-title-container > h1 > a").html(); // Title
-        
+        if(dlbutton.length){
+            // ID
+            record.id = dlbutton.attr("data-deviationid");
+            // URL
+            record.url = dlbutton.attr("href");
+        } else {
+            var img = $("div.dev-view-deviation img.dev-content-normal");
+            // ID
+            record.id = img.attr("data-embed-id");
+            // URL
+            record.url = img.attr("src");
+        }
+
+        // Artist
+        record.artist = location.hostname.substring(0, location.hostname.indexOf("."));
+
+        // Title
+        record.title = $("div.dev-title-container > h1 > a").html();
+
+        // Tags
+        var tags = $("div.dev-title-container div.dev-about-breadcrumb a.discoverytag").map(function(){
+            console.log($(this).attr("data-canonical-tag"));
+            return $(this).attr("data-canonical-tag");
+        }).get();
+        if(tags.length){
+            record.alltags = tags;
+        }
+
+        // Category
+        var cat = $("div.dev-title-container span.dev-about-breadcrumb a.h").last().attr("href");
+        cat = cat.substring(cat.indexOf("all/") + 4);
+        cat = cat.substring(0, cat.length - 1);
+        if(cat.length){
+            record.category = cat;
+        }
+
         // Chech for mature tag
         var mature = $("div.dev-metainfo-details dl dt:contains(Mature Content) ~ dd");
-        if(mature.length && mature.html() == "Yes"){
+        if(mature.length && mature.html() === "Yes"){
             record.tags = [ "mature" ];
         }
-        
+
         console.log(record.site + " - Score: " + record.score + ", ID: " + record.id);
         sendSave(record);
-        
+
     };
-    
+
+    var save_furaffinity = function($, record){
+        record.site = "furaffinity";
+
+        // ID
+        var id = location.pathname.substring(location.pathname.indexOf("view/") + 5);
+        record.id = id.substring(0, id.length - 1);
+
+        // URL
+        record.url = "http:" + $("div.alt1.actions b a:contains(Download)").attr("href");
+
+        // Title
+        record.title = $("td.alt1 table.maintable > tbody > tr > td > b").html();
+
+        // Artist
+        var artist = $("td.alt1 table.maintable > tbody > tr a").attr("href");
+        artist = artist.substring(artist.indexOf("user/") + 5);
+        record.artist = artist.substring(0, artist.length - 1);
+
+        // Search tags
+        var findtags = ["boobs", "nipples", "pussy", "sex", "oral", "anal", "vaginal"];
+
+        // Get keywords / tags
+        record.tags = [];
+        var tags = $("div#keywords a").map(function(){
+            var tag = $(this).attr("href");
+            tag = tag.substring(tag.indexOf(" ") + 1);
+            if(findtags.indexOf(tag) > -1)
+                record.tags.push(tag);
+            return tag;
+        }).get();
+        if(tags.length){
+            record.alltags = tags;
+        }
+
+        console.log(record.site + " - Score: " + record.score + ", ID: " + record.id);
+        sendSave(record);
+    };
+
     checkReady(function($){
         var record = {};
 
@@ -299,12 +367,14 @@
             save_pixiv($, record);
         } else if(location.host.indexOf("deviantart.com") > 0){
             save_deviantart($, record);
+        } else if(location.host === "www.furaffinity.net"){
+            save_furaffinity($, record);
         } else {
             console.error(location.host + " - Unsupported Site");
             record.site = "unknown";
             record.url = location.href;
             sendSave(record);
         }
-        
+
     });
 })();
