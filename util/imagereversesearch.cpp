7f169d44a544557bc9020ee2cfd62bab57912314
Chaos <cawiii@me.com>
2015-04-20 21:56:33 -0400

    curl is bad

--- util/imagereversesearch.cpp
+++ util/imagereversesearch.cpp
@@ -9,8 +9,16 @@ using namespace LibChaos;
 
 // Find image sources
 
-size_t write_data(void *buffer, size_t size, size_t nmemb, void *userp){
+size_t write_callback(void *buffer, size_t size, size_t nmemb, void *userp){
    return size * nmemb;
+}
+
+size_t header_callback(char *buffer, size_t size, size_t nitems, void *userdata){
+    ZBinary bin(buffer, size * nitems);
+    bin.nullTerm();
+    bin.printable();
+    LOG(size << " " << nitems << " " << bin.asChar());
+    return size * nitems;
 }
 
 int main(int argc, char **argv){
@@ -27,17 +35,13 @@ int main(int argc, char **argv){
         return EXIT_FAILURE;
     }
 
-    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
-    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
-
-    LOG("Searching for images");
     zu64 count = 0;
     if(argc < 2){
         LOG("Usage: imagereversesearch <dir>");
         return EXIT_FAILURE;
     }
     ZPath search = argv[1];
+    LOG("Searching for images in " << search);
     ZArray<ZPath> files = ZFile::listFiles(search, true);
     LOG("Testing " << files.size() << " files");
 
@@ -60,31 +64,43 @@ int main(int argc, char **argv){
 
         long http_code = 0;
 
-        //curl_easy_setopt(curl, CURLOPT_URL, ZString("http://e621.net/post/show.json?md5=" + mdhash).cc());
-        //res = curl_easy_perform(curl);
-        //curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
-        //if(res == CURLE_OK && http_code == 200){
-        //    LOG(files[i] << " " << mdhash << " HIT e621 " << http_code);
-        //    ++count;
-        //} else {
-        //    //LOG(files[i] << " " << mdhash << " miss e621 " << http_code);
-        //}
+        ZString url = "http://e621.net/post/show.json?md5=" + mdhash;
+        LOG(files[i] << " " << url << " " << ZLog::noln);
 
-        LOG(files[i] << " " << mdhash << " " << ZLog::noln);
+        curl_easy_setopt(curl, CURLOPT_URL, url.cc());
+        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
 
-        curl_easy_setopt(curl, CURLOPT_URL, ZString("http://rule34.paheal.net/post/list/md5:" + mdhash + "/1").cc());
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
+        //curl_easy_setopt(curl, CURLOPT_HEADERFUNCTION, header_callback);
+        //curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+
         res = curl_easy_perform(curl);
         if(res == CURLE_OK){
             curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
-            if(http_code == 302){
-                LOG("HIT rule34 " << http_code);
+            if(http_code == 200){
+                LOG("HIT e621 " << http_code);
                 ++count;
             } else {
-                LOG("miss rule34");
+                LOG("miss e621 " << http_code);
             }
         } else {
-            LOG("failed rule34");
+            LOG("failed e621: " << curl_easy_strerror(res));
         }
+
+        //curl_easy_setopt(curl, CURLOPT_URL, ZString("http://rule34.paheal.net/post/list/md5:" + mdhash + "/1").cc());
+        //res = curl_easy_perform(curl);
+        //if(res == CURLE_OK){
+        //    curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
+        //    if(http_code == 302){
+        //        LOG("HIT rule34 " << http_code);
+        //        ++count;
+        //    } else {
+        //        LOG("miss rule34");
+        //    }
+        //} else {
+        //    LOG("failed rule34");
+        //}
 
     }
 
